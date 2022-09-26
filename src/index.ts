713a39fea79c5fa3edca7b89b9984fce979e6ae8
takuabonn <k.takumi19980322@gmail.com>
2022-09-26 23:47:17 +0900

    座標 y が座標 z より後なら -1を算出

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+class Hammer {
+  private x: number;
+  private y: number;
+  private z: number;
+
+  constructor(x: number, y: number, z: number) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  getResult = () => {
+    return -1;
+  };
+}
+
+export default Hammer;
---
+++ tests/index.test.ts
@@ -0,0 +1,11 @@
+import Hammer from "../src";
+
+describe("高橋君がゴールに到達することが可能か判定し、可能であれば移動距離の最小値を算出", () => {
+  test("座標 y が座標 z より後なら -1を算出", () => {
+    const X = 100;
+    const Y = 1;
+    const Z = 1000;
+    const hammer = new Hammer(X, Y, Z);
+    expect(hammer.getResult()).toBe(-1);
+  });
+});
